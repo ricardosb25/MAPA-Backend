341de7b5ab344414e4bdf065e623b4309d3cfab4
ricardosb25 <ricardosbissaco@gmail.com>
2026-08-19 01:49:39 -0300

    first commit

---
+++ src/test/java/com/mapa/mapa_api/MapaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapa.mapa_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
